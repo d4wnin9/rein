e5dc4e1bbed7358d9b8c23158d36ab60bc8938c7
d4wnin9 <d4wnin9@gmail.com>
2019-11-05 22:45:42 +0900

    Add cmpThreeInt.s

---
+++ cmpThreeInt/cmpThreeInt.s
@@ -0,0 +1,26 @@
+section .data
+    var1 dq 4
+
+    var2 dq 2
+
+    var3 dq 8
+
+section .text
+
+  global _start
+
+  _start:
+    mov rcx, [var1]
+    cmp rcx, [var2]
+    jg check_third_var
+    mov rcx, [var2]
+
+  check_third_var:
+    cmp rcx, [var3]
+    jg _exit
+    mov rcx, [var3]
+
+  _exit:
+    mov rax, 0x3c
+    mov rdi, rcx
+    syscall
